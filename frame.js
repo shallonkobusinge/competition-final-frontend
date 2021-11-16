c3ebb590c553cc69b5cd1adcf1d6b7d3e960ea2c
shallonkobusinge <kobshallon041@gmail.com>
2021-11-16 22:33:00 +0200

    [FEAT] initialised project

---
+++ frame.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import BASE_URL from '../utils/baseUrl'
+import authHeader from './src/services/authHeader'
+export async function getAllStudents() {
+
+    let students = []
+    await axios.get(`${BASE_URL}/students`, { headers: authHeader() })
+        .then((response) => {
+            console.log(response)
+            for (let student of response?.data?.data) {
+                let data = {
+                    label: `${student?.firstName}  ${student?.lastName}`,
+                    value: student?._id
+                }
+                students.push(data)
+            }
+            return students
+            console.log(response)
+        }).catch((error) => {
+
+        })
+    return students
+
+}
+
+
+export async function getLoggedInUser() {
+
+    let students = []
+    await axios.get(`${BASE_URL}/auth/current-user`, { headers: authHeader() })
+        .then((response) => {
+            console.log(response)
+
+        }).catch((error) => {
+
+        })
+    return students
+
+}
+
+export async function getOneStudent(id) {
+    let studentInfo = []
+    await axios.get(`${BASE_URL}/students/${id}`, { headers: authHeader() })
+        .then((response) => {
+            studentInfo = response?.data?.data
+            return studentInfo
+        })
+        .catch((error) => {
+
+        })
+
+    return studentInfo
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-function App() {
+function PrivateRoute({ children, ...rest }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Route
+      {...rest}
+      render={() => {
+        return sessionStorage.getItem("token") ? (
+          children
+        ) : (
+          <Redirect to="/login" />
+        );
+      }}
+    />
+  );
+}
+
+function PublicRoute({ children, ...rest }) {
+  if (!sessionStorage.getItem("token")) {
+    localStorage.removeItem("user");
+  }
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        return !sessionStorage.getItem("token") ? (
+          children
+        ) : (
+          <Redirect to="/" />
+        );
+      }}
+    />
+  );
+}
+function App() {
+
+  return (
+
+    <Router>
+      <Switch>
+        <h2>Hello World</h2>
+        {/* <PublicRoute exact path="/student-info">
+          <StudentReportInfo />
+        </PublicRoute> */}
+      </Switch>
+    </Router>
   );
 }
 
